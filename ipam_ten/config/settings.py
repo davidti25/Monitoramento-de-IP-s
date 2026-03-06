847698ad30d9255042ea081394b2933d84020ce3
davidti25 <davidbraferti@gmail.com>
2026-03-06 17:00:33 -0300

    Programa operante versão 1.1

--- ipam_ten/config/settings.py
+++ ipam_ten/config/settings.py
@@ -52,12 +52,8 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "ipam_ten"),
-        "USER": os.getenv("DB_USER", "postgres"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "123456"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
